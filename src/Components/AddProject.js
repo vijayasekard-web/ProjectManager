9b5c020f26a2503e5a26e98af960c5569c0a8236
vj-web <vijayasekard.web@gmail.com>
2017-06-04 22:44:57 -0400

    complete project

---
+++ src/Components/AddProject.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+//import ProjectItem from './ProjectItem';
+
+class AddProject extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      newProject: {}
+    }
+  }
+
+  static defaultProps = {
+    categories : ['Web Design', 'Web Development', 'Mobile Development']
+  }
+
+  handleSubmit(e){
+    console.log("Inside Handle " + this.refs.title.value);
+    if(this.refs.title.value === ''){
+      alert("Title is blank")
+    }else{
+      this.setState({newProject: {
+        id: uuid.v4(),
+        title: this.refs.title.value,
+        category: this.refs.category.value 
+
+      }}, function(){
+        // console.log(this.state)
+        this.props.addProject(this.state.newProject)
+      })
+    }
+  }
+
+  render() {
+    let categoryOptions = this.props.categories.map(category => {
+      return <option key={category} value={category}> {category} </option> 
+    });
+    
+    return (
+      <div>
+       <h3> Add Project</h3>
+       <br />
+       <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+            <label>Title</label>
+            <input type="text" ref="title" />
+          </div>
+          <br />
+           <div>
+            <label>Category</label>   <br />
+            <select ref="category"> 
+          {categoryOptions}
+            </select> 
+          </div>
+          <br />
+          
+          <input type="submit" value="SUBMIT"/>
+       </form>
+      </div>
+    );
+  }
+}
+
+AddProject.propTypes = {
+  categories: React.PropTypes.array,
+  addProject: React.PropTypes.func
+}
+
+export default AddProject;
---
+++ src/Components/ProjectItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ProjectItem extends Component {
+  deleteProject(id){
+    this.props.onDelete(id);
+  }
+  render() {
+    console.log(this.props)
+    return (
+      <li className="Project">
+        <strong>{this.props.project.title}</strong> : {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
+      </li>
+    );
+  }
+}
+
+ProjectItem.propTypes = {
+  project: React.PropTypes.object,
+  onDelete: React.PropTypes.func
+}
+export default ProjectItem;
